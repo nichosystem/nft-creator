95e70c6e3899b090eabde962f3fabf3d6910f15e
Nicho <nichosystem@protonmail.com>
2022-07-22 11:40:18 -0500

    Remove LightMode icons

--- apps/frontend/src/components/icons/InstallationIcon.tsx
+++ apps/frontend/src/components/icons/InstallationIcon.tsx
@@ -1,4 +1,4 @@
-import { DarkMode, Gradient, LightMode } from "@/components/Icon";
+import { DarkMode, Gradient } from "@/components/Icon";
 
 export function InstallationIcon({ id, color }: { id: string; color: string }) {
   return (
@@ -15,17 +15,6 @@ export function InstallationIcon({ id, color }: { id: string; color: string }) {
           gradientTransform="matrix(0 21 -21 0 16 7)"
         />
       </defs>
-      <LightMode>
-        <circle cx={12} cy={12} r={12} fill={`url(#${id}-gradient)`} />
-        <path
-          d="m8 8 9 21 2-10 10-2L8 8Z"
-          fillOpacity={0.5}
-          className="fill-[var(--icon-background)] stroke-[color:var(--icon-foreground)]"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </LightMode>
       <DarkMode>
         <path
           d="m4 4 10.286 24 2.285-11.429L28 14.286 4 4Z"
--- apps/frontend/src/components/icons/ThemingIcon.tsx
+++ apps/frontend/src/components/icons/ThemingIcon.tsx
@@ -1,4 +1,4 @@
-import { DarkMode, Gradient, LightMode } from "@/components/Icon";
+import { DarkMode, Gradient } from "@/components/Icon";
 
 export function ThemingIcon({ id, color }: { id: string; color: string }) {
   return (
@@ -15,37 +15,6 @@ export function ThemingIcon({ id, color }: { id: string; color: string }) {
           gradientTransform="matrix(0 24.5 -24.5 0 16 5.5)"
         />
       </defs>
-      <LightMode>
-        <circle cx={12} cy={20} r={12} fill={`url(#${id}-gradient)`} />
-        <path
-          d="M27 12.13 19.87 5 13 11.87v14.26l14-14Z"
-          className="fill-[var(--icon-background)] stroke-[color:var(--icon-foreground)]"
-          fillOpacity={0.5}
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M3 3h10v22a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V3Z"
-          className="fill-[var(--icon-background)]"
-          fillOpacity={0.5}
-        />
-        <path
-          d="M3 9v16a4 4 0 0 0 4 4h2a4 4 0 0 0 4-4V9M3 9V3h10v6M3 9h10M3 15h10M3 21h10"
-          className="stroke-[color:var(--icon-foreground)]"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path
-          d="M29 29V19h-8.5L13 26c0 1.5-2.5 3-5 3h21Z"
-          fillOpacity={0.5}
-          className="fill-[var(--icon-background)] stroke-[color:var(--icon-foreground)]"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </LightMode>
       <DarkMode>
         <path
           fillRule="evenodd"
